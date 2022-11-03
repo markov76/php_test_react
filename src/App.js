c8368347dd223597ed6d593156eceb7b96520ade
markov76 <101133846+markov76@users.noreply.github.com>
2022-11-03 16:42:40 +0200

    json_php_test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const URL = 'http://localhost/php_test/index.php';
 
 function App() {
+  const [origin, setOrigin] = useState('');
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setMessage(response.data.message);
+        setOrigin(response.data.origin);
+     }).catch(error => {
+       alert(error);
+    });
+  }, [] )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Message</h3>
+      <p>{message}</p>
+      <p>osoitteesi on {origin}</p>
     </div>
   );
 }
